67c00e6c96463bb2e248d023529ca1e7d77e9e28
AndreySh <shingaryovan@gmail.com>
2023-02-01 00:27:38 +1200

    копирование массива

---
+++ ex03/Program.cs
@@ -0,0 +1,41 @@
+﻿// копирование массива
+void PrintArray(int[] arg)
+{
+    for (int i = 0; i < arg.Length; i++)
+    {
+        Console.Write(arg[i] + " ");
+    }
+}
+
+void FillArray(int[] arg)
+{
+    for (int i = 0; i < arg.Length; i++)
+    {
+        arg[i] = new Random().Next(1,10);
+    }
+}
+
+int[] CopyArray(int[] arg)
+{
+    int[] Result = new int[arg.Length];
+    for (int i = 0; i < arg.Length; i++)
+    {
+        Result[i] = arg[i];
+    }
+    return Result;
+}
+
+Console.Write("Введите длину массива: ");
+int N = int.Parse(Console.ReadLine());
+int[] array = new int[N];
+
+FillArray(array);
+
+Console.WriteLine("Сгенерирован массив 1: ");
+
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine("Сделана его копия: ");
+PrintArray(CopyArray(array));
+
